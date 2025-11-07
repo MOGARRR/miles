51ed07d34187179548c16ad55526886bf1058e68
TayrineSoares <tayrinecristina@hotmail.com>
2025-11-07 15:19:33 -0500

    add responsiveness to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,42 +5,49 @@ import Image from "next/image";
 const aboutPage = () => {
   return (
     // PAGE CONTAINER 
-    <div className="flex flex-col items-center px-6 py-12 space-y-6">
+    <div className="
+      flex flex-col md:flex-row
+      items-center md:items-start
+      justify-between
+      gap-10
+      px-6 md:px-16 py-12 md:py-20 
+      max-w-6xl mx-auto ">
 
       {/* TEXT + SIGNATURE */}
-      <div className="space-y-4">
-        <h1 className="text-5xl font-bold" >Hi, I'm <span className="text-[#E14747]">Miles</span></h1>
-        <h2 className="text-3xl text-gray-300">The artist behind KiloBoy</h2>
+      {/* flex-1 tells an element to grow and take up all available space inside its flex container */}
+      <div className="flex-1 space-y-5 text-center md:text-left">
+        <h1 className="text-4xl md:text-6xl font-bold" >Hi, I'm <span className="text-[#E14747]">Miles</span></h1>
+        <h2 className="text-xl md:text-3xl text-gray-300">The artist behind KiloBoy</h2>
         <br />
-        <p>
+        <p className="text-base md:text-lg text-gray-200 leading-relaxed">
           I make art that hits with attitude — bold lines, loud colors, and real emotion. Every design comes from a place of passion and energy you can feel.
         </p>
 
-        <p>
+        <p className="text-base md:text-lg text-gray-200 leading-relaxed">
           KiloBoy started as a way for me to express what words couldn't. It's a reflection of the things that inspire me: music, culture, and the people around me. Through art, I get to share moments, moods, and stories that feel real and alive.
         </p>
 
-        <p>
+        <p className="text-base md:text-lg text-gray-200 leading-relaxed">
           At the end of the day, it's about connection. If someone feels something when they see my work, then I've done what I set out to do.
         </p>
         <Image
           src="/images/miles-signature.png"
                     alt="Miles Signature"
-                    width={120}
-                    height={60}
-                    className="mx-auto mt-4">
+                    width={160}
+                    height={80}
+                    className="mx-auto md:mx-0 mt-8">
 
         </Image>
 
       </div>
       
-      <div className="image">
+      <div className="flex-1 flex justify-center md:justify-end">
         <Image
         src="/images/miles.jpg"
                   alt="Miles"
-                  width={400}
-                  height={20}
-                  className="">
+                  width={420}
+                  height={420}
+                  className="rounded-lg object-cover shadow-lg">
 
         </Image>
 
