503f3a1abb854e70bdc0e91dd82c1c97e15e6008
TayrineSoares <tayrinecristina@hotmail.com>
2025-11-07 11:44:29 -0500

    add responsiveness to the footer and improve styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <footer 
       className="
-        footer 
-        footer-center 
+        footer  
+        border-t border-gray-700
         bg-[#1c1c21] text-white
         px-8 py-4 rounded-none"
     >
       
       
-      <div className="flex flex-row justify-between items-center w-full">
+      <div className="flex flex-col md:flex-row justify-between items-center w-full gap-6 md:gap-12 text-center md:text-left">
         <div>
           <Image 
             src="/images/kiloboy-logo.jpg" 
@@ -36,11 +36,11 @@ const Footer = () => {
 
         </div>
 
-        <div className="text-right space-y-1">
+        <div className="text-center md:text-right space-y-1 ">
           <p> (111) 111-1111 </p>
           <p> email@email.com</p>
 
-          <div className="flex justify-end mt-2">
+          <div className="flex justify-center md:justify-end mt-2">
             <a href="https://www.instagram.com/kiloboyartwork/" aria-label="Instagram">
             <Instagram 
               size={24}
