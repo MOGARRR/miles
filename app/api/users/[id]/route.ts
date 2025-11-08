e9c543ce50ce38e63defa0fe783b16933efa436c
Anthony Alicea <alicea.9a@gmail.com>
2025-11-08 13:50:57 -0700

    Feat: Add PUT api route for users

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
-
-
 //GET
 export async function GET(
   req: NextRequest,
@@ -13,7 +11,33 @@ export async function GET(
   let { data, error } = await supabase
     .from("Users")
     .select("*")
-    .eq("id", userId )
+    .eq("id", userId)
+    .single();
+
+  if (error) {
+    console.error("Supabase error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ users: data });
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const userId = params.id;
+  const supabase = await createClient();
+  const updatedUserItem = await req.json();
+  const updates = Object.fromEntries(
+    Object.entries(updatedUserItem) // turn into array of key/values
+      .filter(([_, v]) => v !== undefined) // filter out any undefined values
+  );
+  let { data, error } = await supabase
+    .from("Users")
+    .update(updates)
+    .eq("id", userId)
+    .select()
     .single();
 
   if (error) {
