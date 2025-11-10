5892515d0fcc779e8ff5de1be52ea163b7d5e82d
Anthony Alicea <alicea.9a@gmail.com>
2025-11-10 14:05:36 -0700

    Feat: Add route for api route for categories_products

--- app/api/categories_products/route.ts
+++ app/api/categories_products/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 //GET 
 export async function GET() {
   const supabase = await createClient();
-  let { data, error } = await supabase.from("Categories_products ").select("*");
+  let { data, error } = await supabase.from("Categories_products").select("*");
 
   if (error) {
     console.error("Supabase error:", error);
@@ -12,4 +12,29 @@ export async function GET() {
   }
 
   return NextResponse.json({ users: data });
+}
+
+//Post
+export async function POST(req: NextRequest) {
+  const supabase = await createClient();
+  const categoriesProductsItem = await req.json();
+  let { data, error } = await supabase
+    .from("Categories_products")
+    .insert([
+      {
+      title : categoriesProductsItem.title,
+      description : categoriesProductsItem.description,
+      image_URL : categoriesProductsItem.image_URL,
+      updated_at : categoriesProductsItem.updated_at,
+      },
+    ])
+    .select()
+    .single();
+
+  if (error) {
+    console.error("Supabase error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ categories_products: data });
 }
