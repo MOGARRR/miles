c0327578f391a3d946d81bee31fdd83ce8f47298
Anthony Alicea <alicea.9a@gmail.com>
2025-11-13 11:42:39 -0700

    Feat: Add api route for orders_products and add GET api route

---
+++ app/api/orders_products/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse, NextRequest } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET() {
+  const supabase = await createClient();
+  let { data, error } = await supabase.from("Orders_products").select("*");
+
+  if (error) {
+    console.error("Supabase error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ orders_products: data });
+}
