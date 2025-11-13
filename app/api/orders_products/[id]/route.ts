93f1cdc25f667e721208a70f7b7bbc90f7170d24
Anthony Alicea <alicea.9a@gmail.com>
2025-11-13 12:31:52 -0700

    Feat: Add PUT api route for orders_products

--- app/api/orders_products/[id]/route.ts
+++ app/api/orders_products/[id]/route.ts
@@ -21,3 +21,32 @@ export async function GET(
 
   return NextResponse.json({ orders_products: data });
 }
+
+
+// PUT
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const orders_productsId = params.id;
+  const supabase = await createClient();
+  const updatedOrders_productsItem = await req.json();
+  const updates = Object.fromEntries(
+    Object.entries(updatedOrders_productsItem) // turn into array of key/values
+      .filter(([_, v]) => v !== undefined) // filter out any undefined values
+  );
+
+  let { data, error } = await supabase
+    .from("Orders_products")
+    .update(updates)
+    .eq("id", orders_productsId)
+    .select()
+    .single();
+
+  if (error) {
+    console.error("Supabase error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ orders_products: data });
+}
