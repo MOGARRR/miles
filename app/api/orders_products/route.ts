1e4192d96e8cd904cf60f5bb8beac6f1808c11c9
Anthony Alicea <alicea.9a@gmail.com>
2025-11-13 12:17:43 -0700

    Feat: Add POST api route for orders_products

--- app/api/orders_products/route.ts
+++ app/api/orders_products/route.ts
@@ -12,3 +12,28 @@ export async function GET() {
 
   return NextResponse.json({ orders_products: data });
 }
+
+//Post
+export async function POST(req: NextRequest) {
+  const supabase = await createClient();
+  const orders_productsItem = await req.json();
+  let { data, error } = await supabase
+    .from("Orders_products")
+    .insert([
+      {
+        quantity: orders_productsItem.quantity,
+        unit_price_cents: orders_productsItem.unit_price_cents,
+        order_id: orders_productsItem.order_id,
+        product_id: orders_productsItem.product_id,
+      },
+    ])
+    .select()
+    .single();
+
+  if (error) {
+    console.error("Supabase error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ orders_products: data });
+}
