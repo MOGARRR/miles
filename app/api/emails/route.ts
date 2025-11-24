20a17340935ee021f53b35f4f0124555a28d0da5
Anthony Alicea <alicea.9a@gmail.com>
2025-11-24 10:56:39 -0700

    Feat: Add email api route and basic email functionality

---
+++ app/api/emails/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: Request) {
+  try {
+    const { to, subject, html } = await req.json();
+
+    const data = await resend.emails.send({
+      from:`${process.env.RESEND_FROM_EMAIL}`, 
+      to,
+      subject,
+      html,
+    });
+
+    return NextResponse.json({ success: true, data });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
