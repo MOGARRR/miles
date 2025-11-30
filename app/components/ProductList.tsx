c23676ac8d9849a3f177a5f51800093841d20b6a
TayrineSoares <tayrinecristina@hotmail.com>
2025-11-30 13:59:01 -0500

    Add ProductList component with API fetch and JSON debug output

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,33 +1,23 @@
 import React from "react";
 import ProductListItem from "./ProductListItem";
-import productSetArray from "../data/productData";
+import { Product } from "@/src/types/product";
 
 
-const ProductList = () => {
+const ProductList = async () => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  //fetch from the api route
+  const res = await fetch(`${baseUrl}/api/products`, {cache: "no-store"});
+
+  //parse the JSON
+  const data: { products: Product[] } = await res.json();
+  const products = data.products; // pulls out the array
+
+
   return (
-    <>
-      <div className="p-8">
-        <h1 className="text-3xl mb-5"> All Products</h1>
-        <div className="flex justify-between">
-          <h2 className="text-xl">{productSetArray.length} Products</h2>
-          <h2 className="text-xl underline">Filter & Sort</h2>
-        </div>
-      </div>
-
-      <div className="flex flex-col justify-evenly items-center justify-evenly m-8">
-        {productSetArray.map((set) => (
-          <ProductListItem
-            key={set.id}
-            id={set.id}
-            image={set.image}
-            imageAlt={set.imageAlt}
-            name={set.name}
-            price={set.price}
-          />
-        ))}
-      </div>
-    </>
+    // <pre> is an HTML tag that preserves: whitespace, indentation, line breaks
+    <pre>{JSON.stringify(products, null, 2)}</pre>
   );
-};
+}
 
 export default ProductList;
--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProductList from "../components/ProductList";
-import Directory from "../components/Directory";
+
 
 
 const storePage = () => {
