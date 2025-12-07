8a1706298d906a6d22a6b5676a703aafdda073f8
TayrineSoares <tayrinecristina@hotmail.com>
2025-12-07 15:19:38 -0500

    add remove button on cart page, group items

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,11 +1,34 @@
 "use client"
 import { useCart } from "../components/CartContext";
+import { CartProduct } from "../components/CartContext";
 
+type GroupedCartItem = CartProduct & {
+  quantity: number;
+};
 
 
 const CartPage = () => {
   
-  const { items } = useCart();
+  const { items, removeFromCart } = useCart();
+
+  const groupedItems: GroupedCartItem[] = [];
+
+  for (const item of items) {
+    const existing = groupedItems.find(
+      (groupedItem) => groupedItem.id === item.id
+    );
+
+    if (existing) {
+      existing.quantity += 1;
+    } else {
+      groupedItems.push({
+        ...item,
+        quantity: 1,
+      });
+    }
+  };
+
+
 
   const subtotalCents = items.reduce (
     (sum, item) => sum + item.price_cents, 0 );
@@ -23,22 +46,37 @@ const CartPage = () => {
 
     {/* LIST OF ITEMS  */}
     <ul className="space-y-4 mt-4">
-      {items.map((item) => (
+      {groupedItems.map((item) => (
         
 
-        <li key={item.id}>
+        <li key={item.id} className="flex items-center gap-4 border-b border-gray-700 pb-4">
           <img 
           src={item.image_URL}
           alt={item.title}
           className="w-20 h-20 object-cover rounded-md"
           />
 
-          <div className="flex flex-col">
+          
+
+          <div className="flex flex-col flex-1">
             <span className="font-semibold">{item.title}</span>
+            <span className="text-gray-300 text-sm">
+              Qty: {item.quantity}
+            </span>
             <span className="text-gray-300 text-sm">
               ${(item.price_cents / 100).toFixed(2)}
             </span>
           </div>
+
+          <button 
+            onClick={() => removeFromCart(item.id)}
+            className="text-red-400 hover:text-red-300 underline"
+          >
+            Remove 1 
+          </button>
+
+
+
         </li>
       ))}
     </ul>
