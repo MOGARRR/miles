304f637414e4bbb89b178fd7974ad6338b013654
TayrineSoares <tayrinecristina@hotmail.com>
2025-12-15 18:48:47 -0500

    small cleanup on contact form styling, add contact API route skeleton

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,28 +1,30 @@
-
+"use client"
 
 const ContactPage = () => {
   return (
-    <div className="pt-15">
-      <div className="bg-slate-500 p-4 border-b-2 border-slate text-center place-items-center">
-        <h1 className="text-5xl m-1 border-b-2 pb-1 w-full">Contacts</h1>
+    <div className="">
+      <div className="">
+        <h1 className="text-5xl m-12 ">Contact Us</h1>
         
-        <h1 className="text-xl m-3 border-b-2 pb-4 ">
+        <h1 className="text-xl m-12 ">
           Have any questions? <br /> Reach out and we'll be sure to get back to you as soon as we can.
         </h1>
-        <h2 className="text-2xl mb-4">Get in touch</h2>
-        <fieldset className="fieldset  rounded-box w-xs p-4 text-xl">
+
+        <fieldset className="fieldset p-4 text-xl m-12">
           <label className="label">Name*</label>
-          <input type="text" className="input" />
+          <input type="text" className="border bg-black" />
 
           <label className="label">Email*</label>
-          <input type="email" className="input" />
+          <input type="email" className="border bg-black" />
 
           <label className="label">Subject*</label>
-          <input type="text" className="input" />
+          <input type="text" className="border bg-black" />
 
           <label className="label">Message*</label>
-          <textarea className="textarea" placeholder=""></textarea>
-          <button className=" text-xl text-white btn btn-neutral btn-outline m-5 rounded-sm border-white">
+          <textarea className="border bg-black" placeholder=""></textarea>
+
+          <button 
+            className=" text-xl text-white btn btn-neutral btn-outline m-5 rounded-sm border-white">
             Send
           </button>
         </fieldset>
