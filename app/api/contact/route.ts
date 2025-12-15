02751cd51bdd764c099d564e6b062a43bfd38965
TayrineSoares <tayrinecristina@hotmail.com>
2025-12-15 18:58:14 -0500

    add basic validation to contact API

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,6 +4,18 @@ export async function GET() {
   return NextResponse.json({ ok: true, method: "GET" })
 }
 
-export async function POST() {
-  return NextResponse.json({ ok: true, method: "POST" })
+export async function POST(req: Request) {
+  const { name, email, subject, message } = await req.json();
+
+  if (!name || !email || !subject || !message ) {
+    return NextResponse.json(
+      { ok: false, error: "Missing required fields" },
+      { status: 400 }
+    );
+  }
+
+    return NextResponse.json({ 
+      ok: true, 
+      received: { name, email, subject, message },
+    });
 }
