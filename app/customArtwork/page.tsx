d5ede05743f97431d1d3c8ad8f9593945fc8401b
TayrineSoares <tayrinecristina@hotmail.com>
2025-12-15 20:33:04 -0500

    extract custom request into a component

--- app/customArtwork/page.tsx
+++ app/customArtwork/page.tsx
@@ -1,5 +1,6 @@
-"use client"
+
 import Image from "next/image";
+import CustomArtworkForm from "../components/CustomArtworkForm";
 
 const CustomArtworkPage = () => {
 
@@ -8,12 +9,9 @@ const CustomArtworkPage = () => {
     "/images/custom-artwork2.jpg",
     "/images/custom-artwork3.jpg",
   ];
-
   
 
-  const handleSubmit = async () => {
-
-  }
+  
 
   return (
 
@@ -26,61 +24,7 @@ const CustomArtworkPage = () => {
         <p className="text-xl m-12"> Bring your vision to life with a personalized KiloBoy piece. Whether it's your favorite athlete, character, or moment — let's create something iconic together.</p>
       </div>
       
-      {/* FORM */}
-      <div className="p-4 text-xl m-12">
-        <h2>Request your custom piece</h2>
-        <p>Fill out the form below with as much detail as possible. The more specific you are, the better we can bring your idea to life.</p>
-        <br />
-
-        <form>
-          <fieldset className="fieldset ">
-            <label> Name</label>
-            <input 
-              type="text"
-              required
-              placeholder="Your Name"
-              className="border p-2 bg-black"
-            />
-
-            <label> Email</label>
-            <input 
-              type="text"
-              required
-              placeholder="Your E-mail"
-              className="border p-2 bg-black"
-            />
-
-            <label> Type of Request / Apparel</label>
-            <input 
-              type="text"
-              required
-              placeholder="e.g. T-Shirt, Nike Shoes, Portrait"
-              className="border p-2 bg-black"
-            />
-
-            <label> Specifications</label>
-            <input 
-              type="text"
-              required
-              placeholder="e.g. Size, Colors, Style Preferences"
-              className="border p-2 bg-black"
-            />
-
-            <label> Details </label>
-            <textarea
-              required
-              placeholder="Describe your vision in detail. Include references, inspirations, or specific elements you want included."
-              className="border p-2 bg-black"
-            />
-
-            <button>
-              Submit Request
-            </button>
-            
-          </fieldset>
-        </form>
-        <p className="text-sm ">We'll review your request and get back to you within 2-3 business days with a quote and timeline.</p>
-      </div>
+      <CustomArtworkForm />
 
       {/* CUSTOM WORK INSPIRATION */}
       <div className="p-4 text-xl m-12">
