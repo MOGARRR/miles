64f9e252d2dfca9f1332d9c3b96a5c92e3517682
Anthony Alicea <alicea.9a@gmail.com>
2025-12-18 18:53:53 -0700

    Feat: Add DELETE api route for events

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -1,5 +1,5 @@
-import { updateEvent } from "@/src/controllers/eventsControllers";
 import { NextResponse, NextRequest } from "next/server";
+import { updateEvent, deleteEvent } from "@/src/controllers/eventsControllers";
 
 // PUT
 export async function PUT(
@@ -15,6 +15,26 @@ export async function PUT(
     return NextResponse.json({ event: result });
   } catch (error: any) {
     console.error("Update error:", error);
+    return NextResponse.json(
+      { error: error.message ?? "Unexpected error" },
+      { status: 500 }
+    );
+  }
+}
+
+// DELETE
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const eventId = params.id;
+
+    const result = await deleteEvent(eventId);
+
+    return NextResponse.json({ event: result });
+  } catch (error: any) {
+    console.error("Delete error:", error);
     return NextResponse.json(
       { error: error.message ?? "Unexpected error" },
       { status: 500 }
