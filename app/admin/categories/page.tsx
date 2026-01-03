503a2742f4fa28950672b813cf62be3f40a1fa04
TayrineSoares <tayrinecristina@hotmail.com>
2026-01-03 15:24:31 -0500

    render categoris in categories admin page

--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -1,6 +1,29 @@
 import { headers } from "next/headers";
+import type { Category } from "@/src/types/category";
 
-const AdminCategories = () => {
+
+
+const AdminCategoriesPage = async () => {
+
+  // Get the current host so fetch works in all environments
+  const h = await headers();
+  const host = h.get("host");
+  const protocol = h.get("x-forwarded-proto") ?? "http";
+  const baseUrl = `${protocol}://${host}`;
+
+  const res = await fetch(`${baseUrl}/api/categories_products`, {
+    cache: "no-store", // always show fresh data in admin
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch categories");
+  }
+
+  const data = await res.json(); 
+
+  const categories: Category[] = data.categories_products ?? []; 
+
+
   return (
     <div>
       <h1 className="text-3xl">CATEGORIES ADMIN</h1>
@@ -8,6 +31,21 @@ const AdminCategories = () => {
       <p>
         Manage Product Categories
       </p>
+      {categories.length === 0 ? (
+        <p>No categories found.</p>
+      ) : (
+        <ul>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <p>{category.title}</p>
+
+              {category.description && (
+                <p>{category.description} </p>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
 
 
       
@@ -15,4 +53,4 @@ const AdminCategories = () => {
   )
 };
 
-export default AdminCategories;
+export default AdminCategoriesPage;
