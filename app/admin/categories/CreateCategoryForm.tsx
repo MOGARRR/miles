45ad42494c4b22fcd1758ebf568e95b9959f69b9
TayrineSoares <tayrinecristina@hotmail.com>
2026-01-03 16:11:20 -0500

    add loading animation when creating category

--- app/admin/categories/CreateCategoryForm.tsx
+++ app/admin/categories/CreateCategoryForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import LoadingAnimation from "@/app/components/LoadingAnimation";
 
 const CreateCategoryForm = () => {
 
@@ -9,27 +10,45 @@ const CreateCategoryForm = () => {
   const [title, setTitle] = useState(""); 
   const [description, setDescription] = useState("");
 
+  // state for loading page
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isTitleEmpty = title.trim() === "";
+
   const router = useRouter(); 
 
   const handleSubmit = async (e: React.FormEvent)=> {
     e.preventDefault(); 
 
+    if (isTitleEmpty) return;
+
+    setIsLoading(true);
+
     // Create a new category 
-    await fetch("/api/categories_products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title,
-        description,
-      }),
-    });
+    try {
+      await fetch("/api/categories_products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+        }),
+      });
 
-    // Trigger a re-render of the Server Component 
-    router.refresh();
+      // Trigger a re-render of the Server Component 
+      router.refresh();
 
-  }
+      // Reset form fields
+      setTitle("");
+      setDescription("");
+
+      // Ensures loading state resets even if something goes wrong
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
 
   return (
@@ -60,8 +79,9 @@ const CreateCategoryForm = () => {
 
       <button
         type="submit"
-        className="rounded border p-3 my-6 text-sm">
-        Create Category
+        disabled={isLoading || isTitleEmpty}
+        className="rounded border p-3 my-6 text-sm ">
+        {isLoading ? <LoadingAnimation /> : "Create Category"}
       </button>
 
     </form>
