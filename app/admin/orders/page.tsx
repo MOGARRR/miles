621967bece09ecde445451445d431ad8aa606fc9
Anthony Alicea <alicea.9a@gmail.com>
2026-01-21 18:51:04 -0700

    Refactor/WIP: Create client component and order data type to refactor admin order page

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,16 +1,13 @@
 // "use client";
 import { getBaseUrl } from "@/src/helpers/getBaseUrl";
+import AdminOrdersClient from "./AdminOrdersClient";
 
 const AdminOrders = async () => {
   const baseUrl = await getBaseUrl();
-  const orderRes = await fetch(`${baseUrl}/api/orders`, {
-    cache: "no-store",
-  });
+  const orderRes = await fetch(`${baseUrl}/api/orders`, {cache: "no-store",});
 
-  if (!orderRes.ok) {
-    throw new Error("Failed to fetch products");
-  }
-
+  if (!orderRes.ok) {throw new Error("Failed to fetch products");}
+  
   const orderData = await orderRes.json();
 
   return (
@@ -20,48 +17,9 @@ const AdminOrders = async () => {
         <p>Manage Orders</p>
       </div>
       <br />
+      {/* <pre>{JSON.stringify(orderData.orders, null, 2)}</pre> */}
       {orderData.orders.map((order: any) => (
-        <div className="bg-stone-800 m-5 p-5" key={order.id}>
-          <div>
-            <h1>Order ID: {order.id}</h1>
-            <p>Status: {order.status}</p>
-          </div>
-          <br />
-          <div className="flex flex-col">
-            <div className="">
-              <h1>Customer Info:</h1>
-              <p>Full name: {order.full_name}</p>
-              <p>Email: {order.email}</p>
-              <p>Phone Number: {order.phone_number}</p>
-            </div>
-            <br />
-
-            <div className="">
-              <h1>Payment Info:</h1>
-              <p>Stripe_session_id: {order.stripe_session_id}</p>
-              <p>Payment status: {order.payment_status}</p>
-              <p>Total_cents: ${order.total_cents / 100}</p>
-            </div>
-          </div>
-          <br />
-
-          <div className="">
-            <h1>Shipping Info:</h1>
-            <p>Shipping Status: {order.shipping_status}</p>
-            <p>Tracking Number: {order.tracking_number}</p>
-            <p>ETA: {order.estimated_delivery}</p>
-            <p>Address line 1: {order.address_line_1}</p>
-            <p>Address line 2: {order.address_line_2}</p>
-            <p>Postal: {order.postal}</p>
-            <p>City: {order.city}</p>
-            <p>Province: {order.province}</p>
-          </div>
-          <br />
-          <div>
-            <p>Order Created: {order.created_at}</p>
-            <p>Last Updated: {order.updated_at}</p>
-          </div>
-        </div>
+        <AdminOrdersClient key={order.id} order={order}/>
       ))}
     </div>
   );
