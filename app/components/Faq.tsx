4e0879c5b25d9e260e7d96899705518592c95caa
TayrineSoares <tayrinecristina@hotmail.com>
2026-01-31 13:41:58 -0500

    style FAQ

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -1,35 +1,46 @@
 
+const questions = [
+  {q: "How long does a custom piece take?", answer:"Timelines vary depending on complexity, but most custom pieces are completed within 2-4 weeks after approval."},
+
+  {q: "What's the pricing for custom work?", answer:"Pricing depends on the size, medium, and complexity of the artwork. We'll provide a detailed quote after reviewing your request."},
+
+  {q: "Can I request revisions?", answer:"Pricing depends on the size, medium, and complexity of the artwork. We'll provide a detailed quote after reviewing your request."},
+
+  {q: "SAMPLE QUESTION", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus rutrum neque vel luctus."},
+
+
+]
 
 const Faq = () => {
   return (
-    <section className="
-      max-w-7xl mx-auto
-      px-6 md:px-16 py-24"
-    >
-      <h1 className="text-4xl"> FREQUENTLY ASKED QUESTIONS </h1>
-      <br />
+    <section className="">
       
-      <div>
-        <h2 className="text-2xl" >How long does a custom piece take?</h2>
-        <p>Timelines vary depending on complexity, but most custom pieces are completed within 2-4 weeks after approval.</p>
+      <div className="
+        max-w-[768px] mx-auto 
+        px-6 md:px-16 py-16
 
-        <br/> <br/> 
+        ">
+         <h2 className="text-3xl mb-4 text-kilored text-center"> 
+          FREQUENTLY ASKED QUESTIONS 
+        </h2>
+    
+    
+        {questions.map((question) => (
+          <div className="
+          bg-kilodarkgrey 
+          rounded-lg border border-[#3a3a41]
+          my-8 p-6">
+          <h2 className="text-xl text-kilotextlight mb-2">
+            {question.q}
+          </h2>
 
-        <h2 className="text-2xl" >What's the pricing for custom work?</h2>
-        <p>Pricing depends on the size, medium, and complexity of the artwork. We'll provide a detailed quote after reviewing your request.</p>
-
-        <br/> <br/> 
-
-        <h2 className="text-2xl" >Can I request revisions?</h2>
-        <p>Pricing depends on the size, medium, and complexity of the artwork. We'll provide a detailed quote after reviewing your request.</p>
-
-        <br/> <br/> 
-
-        <h2 className="text-2xl" >SAMPLE QUESTION</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus rutrum neque vel luctus.</p>
-
+          <p className="text-base text-kilotextgrey">{question.answer}</p>
+          </div>
+        ))}
 
       </div>
+     
+
     </section>
   )
 };
