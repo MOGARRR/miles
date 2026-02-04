6fde96f2ec22f76cbb0a3e8966d51c46f10852eb
Anthony Alicea <alicea.9a@gmail.com>
2026-02-04 15:24:55 -0700

    Feat: Add logout button and function for admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,13 @@
+import { logoutUser } from "../actions/logoutUser";
 
 const AdminPage = () => {
   return (
     <div>
       <h1>I am the admin main page</h1>
+       <button
+        className="bg-gray-400 p-2 rounded-lg"
+        onClick={logoutUser}
+       >Logout</button>
 
     </div>
   )
