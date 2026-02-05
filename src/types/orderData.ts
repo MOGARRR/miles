ea20df4ec60d50b675ee09c662ab1868d84cdc20
Anthony Alicea <alicea.9a@gmail.com>
2026-02-05 16:07:51 -0700

    WIP: Create data type for order update form

--- src/types/orderData.ts
+++ src/types/orderData.ts
@@ -1,27 +1,27 @@
 import { Product } from "./product";
 
 export interface orderData {
-  id: number,
-  total_cents: number,
-  stripe_session_id: string,
-  status: string,
-  created_at: string,
-  updated_at: string,
-  payment_status: string,
-shipping_fee_cents: number,
-tracking_number: string,
-label_url: string,
-estimated_delivery: any,
-shipping_status: string,
-full_name: string,
-address_line_1: string,
-address_line_2: string,
-postal: string,
-city: string,
-province:string,
-email: string,
-phone_number: string
- order_products: OrderProduct[];
+  id: number;
+  total_cents: number;
+  stripe_session_id: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  payment_status: string;
+  shipping_fee_cents: number;
+  tracking_number: string;
+  label_url: string;
+  estimated_delivery: any;
+  shipping_status: string;
+  full_name: string;
+  address_line_1: string;
+  address_line_2: string;
+  postal: string;
+  city: string;
+  province: string;
+  email: string;
+  phone_number: string;
+  order_products: OrderProduct[];
 }
 
 export type OrderProduct = {
---
+++ src/types/orderFormData.ts
@@ -0,0 +1,17 @@
+export interface orderFormData {
+   customerName: string,
+    address1: string,
+    address2: string,
+    postal: string,
+    city: string,
+    province:string,
+    email: string,
+    phone: string,
+    shippingStatus: string,
+    trackingNumber: string,
+    labelUrl: string,
+    estimatedDelivery: string,
+}
+
+
+
