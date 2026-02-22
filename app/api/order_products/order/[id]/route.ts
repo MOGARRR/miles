9aed86a2cdff25e392f75998b8132e06a2288b9b
Anthony Alicea <alicea.9a@gmail.com>
2026-02-21 19:51:32 -0700

    Fix/Temp: Update order_products/orders api route

--- app/api/order_products/order/[id]/route.ts
+++ app/api/order_products/order/[id]/route.ts
@@ -1,16 +1,15 @@
 import { NextResponse, NextRequest } from "next/server";
 import { getOrderProductByOrderId } from "@/src/controllers/order_productsControllers";
 import { getOrderById } from "@/src/controllers/orderControllers";
+import { RouteContext } from "@/src/types/routeContext";
 
 //GET Order data and its product data 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: NextRequest, context: RouteContext){
   try {
-    const order = await getOrderById(params.id);
+    const { id } = await context.params;
+    const order = await getOrderById(id);
     try {
-      const order_product = await getOrderProductByOrderId(params.id);
+      const order_product = await getOrderProductByOrderId(id);
       return NextResponse.json({ order, order_product }, { status: 200 });
     } catch (error: any) {
       console.error("GET /api/order_product error:", error.message);
