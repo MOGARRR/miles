6b9d3bdbd37537d80a67b0bbf725553d0369d750
Anthony Alicea <alicea.9a@gmail.com>
2026-02-21 18:14:46 -0700

    Fix/Temp: Test update Get route with nextjs v15 route preference

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -3,11 +3,12 @@ import { deleteCategoriesProducts, getCategorieProductById, updateCategoriesProd
 
 //GET
 export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
+  request: Request,
+  context: { params: { id: string } }
 ) {
-  const {id} = params;
+
   try {
+    const id = context.params.id;
     const categories = await getCategorieProductById(id);
     return NextResponse.json({ categories }, { status: 200 });
   } catch (error: any) {
